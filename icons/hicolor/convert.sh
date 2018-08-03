fa1893347a72c3adff4c3f8ce488d4c92c0c4972
Stasiek Michalski <hellcp@opensuse.org>
2018-08-03 05:58:09 +0200

    Modify script to take only new icons into consideration

--- icons/hicolor/convert.sh
+++ icons/hicolor/convert.sh
@@ -2,7 +2,7 @@
 for x in 16 22 32 48 64 256
 do
   mkdir -p ${x}x${x}/apps
-  for name in `ls scalable/apps/ | grep ".svg" | sed 's/.svg//g'`
+  for name in `cat new-icons.txt`
   do
     inkscape -z --export-png ${x}x${x}/apps/${name}.png scalable/apps/${name}.svg -w ${x} 2> /dev/null 1> /dev/null
   done
