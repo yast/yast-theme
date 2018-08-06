e3c90f13fcaa24b1cce1ddf5332033a587a1aacc
Stasiek Michalski <hellcp@opensuse.org>
2018-08-06 15:00:04 +0200

    Add comment and sort new-icons.txt

--- icons/hicolor/convert.sh
+++ icons/hicolor/convert.sh
@@ -2,7 +2,7 @@
 for x in 16 22 32 48 64 256
 do
   mkdir -p ${x}x${x}/apps
-  for name in `cat new-icons.txt`
+  for name in `grep -v '^#' new-icons.txt`
   do
     inkscape -z --export-png ${x}x${x}/apps/${name}.png scalable/apps/${name}.svg -w ${x} 2> /dev/null 1> /dev/null
   done
