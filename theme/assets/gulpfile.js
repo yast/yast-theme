846d1168ddc9ad66987f01ade95f713ff7abfb8a
Ladislav Slezák <lslezak@suse.cz>
2022-01-28 13:19:46 +0100

    Added stylelint

--- theme/assets/gulpfile.js
+++ theme/assets/gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const gulpStylelint = require('gulp-stylelint');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
 
@@ -8,7 +9,7 @@ let path = {
   src_destination: '../SLE/wizard/',
 }
 
-gulp.task("theme-dark", function() {
+gulp.task('theme-dark', function() {
   return gulp
     .src(path.src_sass)
     .pipe(sass().on('error', sass.logError))
@@ -16,12 +17,34 @@ gulp.task("theme-dark", function() {
     .pipe(gulp.dest(path.src_destination))
 });
 
-gulp.task("theme-light", function() {
+gulp.task('theme-light', function() {
   return gulp
     .src(path.src_sass_light)
     .pipe(sass().on('error', sass.logError))
     .pipe(rename({ extname: '.qss' }))
     .pipe(gulp.dest(path.src_destination))
 });
+
+gulp.task('lint-dark-css', function () {
+  return gulp
+    .src('../SLE/wizard/installation.qss')
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
+});
+
+gulp.task('lint-light-css', function () {
+  return gulp
+    .src('../SLE/wizard/installation-light.qss')
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
+});
+
+gulp.task('lint-css', gulp.series('lint-dark-css', 'lint-light-css'));
 
 gulp.task('default', gulp.series('theme-dark', 'theme-light'));
