8c32b06de04de0a7ad4a74e1e873e4ddddd14c84
David Díaz González <dgonzalez@suse.de>
2022-02-24 14:21:02 +0000

    Adapt gulpfile for building themes dinamically
    That way, the _richtext.css file can be dinamically generated for each
    theme too.

--- theme/assets/gulpfile.js
+++ theme/assets/gulpfile.js
@@ -2,55 +2,57 @@ const gulp = require('gulp');
 const gulpStylelint = require('gulp-stylelint');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
+const header = require('gulp-header');
 
 let path = {
-  src_sass: 'scss/installation.scss',
-  src_sass_light: 'scss/installation-light.scss',
-  src_sass_wb: 'scss/white-black.scss',
-  src_sass_cb: 'scss/cyan-black.scss',
-  src_sass_highcontrast: 'scss/highcontrast.scss',
+  src_source: 'scss',
   src_destination: '../SLE/wizard/',
 }
 
-gulp.task('theme-dark', function() {
-  return gulp
-    .src(path.src_sass)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename({ extname: '.qss' }))
-    .pipe(gulp.dest(path.src_destination))
-});
+const themes = [
+  'installation',
+  'installation-light',
+  'white-black',
+  'cyan-black',
+  'highcontrast'
+];
 
-gulp.task('theme-light', function() {
-  return gulp
-    .src(path.src_sass_light)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename({ extname: '.qss' }))
-    .pipe(gulp.dest(path.src_destination))
-});
+const buildThemes = (done) => {
+  const tasks = themes.flatMap((theme) => {
+    function buildQSS() {
+      return gulp
+        .src(`${path.src_source}/${theme}.scss`)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(rename({ extname: '.qss' }))
+        .pipe(gulp.dest(path.src_destination))
+    }
 
-gulp.task('theme-white-black', function() {
-  return gulp
-    .src(path.src_sass_wb)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename({ extname: '.qss' }))
-    .pipe(gulp.dest(path.src_destination))
-});
+    function buildRichtextCSS() {
+      let yastTheme = theme;
 
-gulp.task('theme-cyan-black', function() {
-  return gulp
-    .src(path.src_sass_cb)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename({ extname: '.qss' }))
-    .pipe(gulp.dest(path.src_destination))
-});
+      if(theme === 'installation') yastTheme = 'dark';
+      if(theme === 'installation-light') yastTheme = 'light';
 
-gulp.task('theme-highcontrast', function() {
-  return gulp
-    .src(path.src_sass_highcontrast)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename({ extname: '.qss' }))
-    .pipe(gulp.dest(path.src_destination))
-});
+      return gulp
+        .src(`${path.src_source}/richtext.scss`)
+        // Sass imports can't use interpolation, see https://sass-lang.com/documentation/at-rules/import#interpolation
+        .pipe(header(`@import 'theme-vars/${yastTheme}';\n`))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(rename({ basename: `${theme}_richtext`, extname: '.css' }))
+        .pipe(gulp.dest(path.src_destination))
+    }
+
+    buildQSS.displayName = `buildQSS for '${theme}' theme`
+    buildRichtextCSS.displayName = `buildRichtextCSS for '${theme}' theme`
+
+    return [buildQSS, buildRichtextCSS];
+  });
+
+  return gulp.series(...tasks, (seriesDone) => {
+    seriesDone();
+    done();
+  })();
+};
 
 gulp.task('lint-css', function () {
   return gulp
@@ -74,10 +76,4 @@ gulp.task('lint-scss', function () {
 
 gulp.task('lint', gulp.series('lint-scss', 'lint-css'));
 
-gulp.task('default', gulp.series(
-  'theme-cyan-black',
-  'theme-dark',
-  'theme-highcontrast',
-  'theme-light',
-  'theme-white-black'
-));
+gulp.task('default', gulp.series(buildThemes));
